86ecc44ccf070d9b7b954e30f1067692c69787f1
Mr. MR <mohammad117811@gmail.com>
2025-02-21 15:54:36 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,24 @@
 self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/index.css",
+        "/index.js",
+        "/icons/Chemistry-Lab-icon-192x192.png",
+        "/icons/Chemistry-Lab-icon-512x512.png"
+      ]);
+    })
+  );
   console.log("Service Worker نصب شد.");
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("درخواست دریافت شد:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
