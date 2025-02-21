d49cf9d8e98ffc2647b96750d65126269e23833c
Mr. MR <mohammad117811@gmail.com>
2025-02-21 18:47:36 +0330

    Update index.js

--- Atom-Lab/index.js
+++ Atom-Lab/index.js
@@ -1446,7 +1446,7 @@ function searchElement() {
         const elementSymbolLower = element.symbol.toLowerCase();
 
         if (
-            atomicNumber === query || 
+            atomicNumber === query ||
             elementSymbolLower === query ||  // بررسی دقیق نماد شیمیایی
             elementNameLower.startsWith(query)
         ) {
@@ -1472,26 +1472,26 @@ searchBox.addEventListener("input", searchElement);
 updateElement();
 
 function applyTheme(theme) {
-  const isLightTheme = theme === "light";
-  document.body.classList.toggle("light-theme", isLightTheme);
-  
-  const color = isLightTheme ? "#e2e2e2" : "#0f172a"; // رنگ برای حالت روز و شب
-  document.querySelector('meta[name="theme-color"]').setAttribute("content", color);
-  document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute("content", theme === "dark" ? "black-translucent" : "default");
-  document.querySelector('meta[name="msapplication-navbutton-color"]').setAttribute("content", color);
-  document.querySelector('meta[name="msapplication-TileColor"]').setAttribute("content", color);
+    const isLightTheme = theme === "light";
+    document.body.classList.toggle("light-theme", isLightTheme);
+
+    const color = isLightTheme ? "#e2e2e2" : "#0f172a"; // رنگ برای حالت روز و شب
+    document.querySelector('meta[name="theme-color"]').setAttribute("content", color);
+    document.querySelector('meta[name="apple-mobile-web-app-status-bar-style"]').setAttribute("content", theme === "dark" ? "black-translucent" : "default");
+    document.querySelector('meta[name="msapplication-navbutton-color"]').setAttribute("content", color);
+    document.querySelector('meta[name="msapplication-TileColor"]').setAttribute("content", color);
 }
 
 // اجرا هنگام بارگذاری صفحه برای اطمینان از اینکه Title Bar همیشه هماهنگ است
 function loadTheme() {
-  let savedTheme = localStorage.getItem("theme") || "dark";
-  applyTheme(savedTheme);
+    let savedTheme = localStorage.getItem("theme") || "dark";
+    applyTheme(savedTheme);
 }
 
 // اجرا هنگام بارگذاری هر صفحه
 document.addEventListener("DOMContentLoaded", loadTheme);
 window.addEventListener("pageshow", function (event) {
-  if (event.persisted) { // اگر صفحه از کش لود شده بود، دوباره تم را اعمال کن
-      loadTheme();
-  }
+    if (event.persisted) { // اگر صفحه از کش لود شده بود، دوباره تم را اعمال کن
+        loadTheme();
+    }
 });
