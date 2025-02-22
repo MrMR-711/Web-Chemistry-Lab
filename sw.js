540cd124eda66a95f66447fc7c98b60542ad54cd
Mr. MR <mohammad117811@gmail.com>
2025-02-22 10:47:21 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,7 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("v1").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/css/index.css",
-        "/index.js",
-        "/icons/Chemistry-Lab-icon-192x192.png",
-        "/icons/Chemistry-Lab-icon-512x512.png"
-      ]);
-    })
-  );
-  console.log("Service Worker نصب شد.");
-  self.skipWaiting();
-});
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return new Response('', { status: 503, statusText: "Service Unavailable" });
     })
   );
 });
