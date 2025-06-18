4807999213405336ee4649a60c71f33b41e5c050
MrMR-711 <mohammad117811@gmail.com>
2025-06-18 15:11:57 +0330

    1.2.0v

--- Ai-Solver/Fa/index.js
+++ Ai-Solver/Fa/index.js
@@ -396,3 +396,15 @@ if ('serviceWorker' in navigator) {
     .then(() => console.log("Service Worker ثبت شد."))
     .catch(err => console.error("خطا در ثبت Service Worker:", err));
 }
+
+function launchFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+}
+
+document.addEventListener('click', () => {
+  launchFullscreen(document.documentElement);
+});
